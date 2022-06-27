2031114c266cf4cf282977430872b7d564660840
Krutika77 <40793266+Krutika77@users.noreply.github.com>
2022-06-27 16:46:59 -0400

    Local Storage

--- src/contexts/ListContext.js
+++ src/contexts/ListContext.js
@@ -1,10 +1,16 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import { listReducer } from "../reducers/listReducer";
 
 export const ListContext = createContext();
 
 const ListContextProvider = (props) => {
-  const [todos, dispatch] = useReducer(listReducer, []);
+  const [todos, dispatch] = useReducer(listReducer, [], () => {
+    const localData = localStorage.getItem("todos");
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
     <ListContext.Provider value={{ todos, dispatch }}>
       {props.children}
